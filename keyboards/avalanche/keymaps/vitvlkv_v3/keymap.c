7a2da53ae5cf03e9aff343906bba6ed972824196
vitvlkv <vitvlkv@gmail.com>
2021-09-19 00:48:49 +0300

    This is needed for Russian letters 'х' and 'ъ'

--- keyboards/avalanche/keymaps/vitvlkv_v3/keymap.c
+++ keyboards/avalanche/keymaps/vitvlkv_v3/keymap.c
@@ -16,7 +16,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LAYER_0] = LAYOUT(
                  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                                       KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
                  KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-        XXXXXXX, KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, XXXXXXX,
+        KC_LBRC, KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_RBRC,
                  KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_DEL,  KC_APP,  KC_PSCR,  KC_BSPC,KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RCTL,
                                             KC__MUTE,MT_GRV,  KC_LSFT, MT_SPC,  MT_LALT, MT_RALT,  FN_1,   KC_RSFT, KC_TILD, KC_INS
     ),
