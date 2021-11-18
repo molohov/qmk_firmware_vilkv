7b7113dc2f395e9196844b2a2aae11c9cc741bb5
Bryce Long <longbryc@gmail.com>
2021-11-18 10:21:06 -0800

    Move screenshot macro to left side Move 5 to index finger Add some more symbol combos

--- keyboards/avalanche/keymaps/molohov/keymap.c
+++ keyboards/avalanche/keymaps/molohov/keymap.c
@@ -155,7 +155,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_HANDS_DOWN] = LAYOUT(
                 VS_EDIT,    KC_1,       KC_2,       KC_3,       KC_4,       KC_5,                               KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       VS_TERM,
                 VS_CTLP,    KC_X,       KC_F,       KC_M,       KC_P,       KC_B,                               KC_MINS,    KC_SLSH,    KC_DOT,     KC_COMM,    KC_Q,       LNX_RSR,
-    VIPASTE,    KC_Z,       KC_R,       KC_S,       HRM_N,      HRM_T,      KC_G,       PC_LOCK,    KC_MPLY,    KC_QUOT,    HRM_A,      HRM_E,      HRM_I,      HRM_H,      KC_J,       PC_SCSH,
+    PC_SCSH,    KC_Z,       KC_R,       KC_S,       HRM_N,      HRM_T,      KC_G,       PC_LOCK,    KC_MPLY,    KC_QUOT,    HRM_A,      HRM_E,      HRM_I,      HRM_H,      KC_J,       VIPASTE,
                 KC_HOME,    KC_W,       KC_C,       KC_L,       KC_D,       KC_V,       PC_BSWD,    PC_SLACK,   KC_EQL,     KC_U,       KC_O,       KC_Y,       KC_K,       KC_END,
                                         NUMTOG,     VS_COMT,    GUIDEL,     SFTBSP,     CTLTAB,     ALTENT,     SPCNAV,     ESCNUM,     PC_CLIP,    QWERTY_GAME
     ),
@@ -164,7 +164,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,                              KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,
                 XXXXXXX,    KC_AMPR,    KC_GRV,     KC_HASH,    KC_LBRC,    KC_RBRC,                            KC_MINS,    LNX_LWD,    KC_UP,      LNX_RWD,    XXXXXXX,    XXXXXXX,
     KC_X,       KC_BSLS,    KC_COLN,    KC_PERC,    KC_PAST,    KC_LPRN,    KC_RPRN,    _______,    _______,    KC_QUOT,    KC_LEFT,    KC_DOWN,    KC_RGHT,    KC_COMM,    KC_SLSH,    KC_SLSH,
-                KC_EQL,     KC_7,       KC_5,       KC_1,       KC_3,       KC_9,       _______,    _______,    KC_8,       KC_2,       KC_0,       KC_4,       KC_6,       KC_DOT,
+                KC_EQL,     KC_7,       KC_3,       KC_1,       KC_5,       KC_9,       _______,    _______,    KC_8,       KC_2,       KC_0,       KC_4,       KC_6,       KC_DOT,
                                         _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______
     ),
 
@@ -344,13 +344,16 @@ enum combo_events {
     COMBO_PC_FIND,
     COMBO_PC_UNDO,
     COMBO_PC_SELECTALL,
-    COMBO_LNX_RSRCH,
+    COMBO_WIN_BLUETOOTH,
+    COMBO_LNX_CLS,
     COMBO_LNX_LAST,
     COMBO_KU_QU,
     COMBO_EXCLAMATION,
     COMBO_AT,
+    COMBO_DOLLAR,
     COMBO_DEL_WORD,
     COMBO_SEMICOLON,
+    COMBO_ESC,
     COMBO_LENGTH
 };
 uint16_t COMBO_LEN = COMBO_LENGTH;
@@ -362,6 +365,7 @@ uint16_t COMBO_LEN = COMBO_LENGTH;
 // const uint16_t PROGMEM vimquitall[] =   {KC_N, KC_B, COMBO_END};
 // const uint16_t PROGMEM vimshiftv[] =    {KC_N, KC_P, COMBO_END};
 
+// LEFT HAND
 // copy/paste combos on left bottom row
 const uint16_t PROGMEM pc_undo[] =      {KC_W, KC_C, COMBO_END};
 const uint16_t PROGMEM pc_selectall[] = {KC_W, KC_S, COMBO_END};
@@ -370,48 +374,56 @@ const uint16_t PROGMEM pc_cut[] =       {KC_C, KC_N, COMBO_END};
 const uint16_t PROGMEM pc_paste[] =     {KC_L, KC_D, COMBO_END};
 const uint16_t PROGMEM pc_find[] =      {KC_L, KC_T, COMBO_END};
 
+// this combo mirrors the DW action in vim!
+const uint16_t PROGMEM del_word[] =     {KC_W, KC_D,    COMBO_END};
+// keep shift+V on the left side
+const uint16_t PROGMEM vimshiftv[] =    {KC_N, KC_D, COMBO_END};
+const uint16_t PROGMEM escape[] =       {KC_F, KC_M, COMBO_END};
+
+
+// RIGHT HAND
 // vim combos rooted from right home row middle finger
 const uint16_t PROGMEM vimwrite[] =     {KC_E, KC_A,    COMBO_END};
 const uint16_t PROGMEM vimwritequit[] = {KC_E, KC_U,    COMBO_END};
 const uint16_t PROGMEM vimquit[] =      {KC_E, KC_QUOT, COMBO_END};
 const uint16_t PROGMEM vimquitall[] =   {KC_E, KC_MINS, COMBO_END};
-// keep shift+V on the left side
-const uint16_t PROGMEM vimshiftv[] =    {KC_N, KC_D, COMBO_END};
-// const uint16_t PROGMEM vimshiftv[] =    {KC_E, KC_SLSH, COMBO_END};
 
-// linux combos rooted from right home row ring finger
-const uint16_t PROGMEM lnx_rsearch[] =  {KC_I, KC_E,    COMBO_END};
+// linux combos
+const uint16_t PROGMEM lnx_cls[] =      {KC_Y, KC_O,    COMBO_END};
 const uint16_t PROGMEM lnx_last[] =     {KC_I, KC_DOT,  COMBO_END};
 
 const uint16_t PROGMEM ku_qu[] =        {KC_U, KC_K,    COMBO_END};
-// const uint16_t PROGMEM shft_ku_qu[] =   {KC_LSFT, KC_U, KC_K,    COMBO_END};
 
-const uint16_t PROGMEM exclamation[] =  {KC_A, KC_DOT,    COMBO_END};
-const uint16_t PROGMEM at[] =           {KC_SLSH, KC_DOT,    COMBO_END};
+const uint16_t PROGMEM exclamation[] =  {KC_DOT, KC_A,    COMBO_END};
+const uint16_t PROGMEM at[] =           {KC_DOT, KC_SLSH,   COMBO_END};
+const uint16_t PROGMEM dollar[] =       {KC_DOT, KC_MINS,    COMBO_END};
 // . + , = ;
 const uint16_t PROGMEM semicolon[] =    {KC_DOT, KC_COMM,    COMBO_END};
+const uint16_t PROGMEM win_bluetooth[] =  {KC_K, KC_Y, COMBO_END};
 
-// this combo mirrors the DW action in vim!
-const uint16_t PROGMEM del_word[] =     {KC_W, KC_D,    COMBO_END};
 
 combo_t key_combos[] = {
-    [COMBO_VIM_WRITE]     = COMBO(vimwrite,       VIMWRITE),
-    [COMBO_VIM_WRITEQUIT] = COMBO(vimwritequit,   VIMWRITEQUIT),
-    [COMBO_VIM_QUIT]      = COMBO(vimquit,        VIMQUIT),
-    [COMBO_VIM_QUITALL]   = COMBO(vimquitall,     VIMQUITALL),
-    [COMBO_VIM_SHIFTV]    = COMBO(vimshiftv,      S(KC_V)),
-    [COMBO_PC_COPY]       = COMBO(pc_copy,        PC_COPY),
-    [COMBO_PC_CUT]        = COMBO(pc_cut,         PC_CUT),
-    [COMBO_PC_PASTE]      = COMBO(pc_paste,       PC_PASTE),
-    [COMBO_PC_FIND]       = COMBO(pc_find,        PC_FIND),
-    [COMBO_PC_UNDO]       = COMBO(pc_undo,        PC_UNDO),
-    [COMBO_PC_SELECTALL]  = COMBO(pc_selectall,   PC_SALL),
-    [COMBO_LNX_LAST]      = COMBO(lnx_last,       LNX_LAST),
-    [COMBO_KU_QU]         = COMBO(ku_qu,          KU_QU),
-    [COMBO_EXCLAMATION]   = COMBO(exclamation,    KC_EXLM),
-    [COMBO_AT]            = COMBO(at,             KC_AT),
-    [COMBO_SEMICOLON]     = COMBO(semicolon,      KC_SCLN),
-    [COMBO_DEL_WORD]      = COMBO(del_word,       C(KC_DEL)),
+    [COMBO_VIM_WRITE]       = COMBO(vimwrite,       VIMWRITE),
+    [COMBO_VIM_WRITEQUIT]   = COMBO(vimwritequit,   VIMWRITEQUIT),
+    [COMBO_VIM_QUIT]        = COMBO(vimquit,        VIMQUIT),
+    [COMBO_VIM_QUITALL]     = COMBO(vimquitall,     VIMQUITALL),
+    [COMBO_VIM_SHIFTV]      = COMBO(vimshiftv,      S(KC_V)),
+    [COMBO_PC_COPY]         = COMBO(pc_copy,        PC_COPY),
+    [COMBO_PC_CUT]          = COMBO(pc_cut,         PC_CUT),
+    [COMBO_PC_PASTE]        = COMBO(pc_paste,       PC_PASTE),
+    [COMBO_PC_FIND]         = COMBO(pc_find,        PC_FIND),
+    [COMBO_PC_UNDO]         = COMBO(pc_undo,        PC_UNDO),
+    [COMBO_PC_SELECTALL]    = COMBO(pc_selectall,   PC_SALL),
+    [COMBO_WIN_BLUETOOTH]   = COMBO(win_bluetooth,  G(KC_K)),
+    [COMBO_LNX_LAST]        = COMBO(lnx_last,       LNX_LAST),
+    [COMBO_LNX_CLS]         = COMBO(lnx_cls,        C(KC_L)),
+    [COMBO_KU_QU]           = COMBO(ku_qu,          KU_QU),
+    [COMBO_EXCLAMATION]     = COMBO(exclamation,    KC_EXLM),
+    [COMBO_AT]              = COMBO(at,             KC_AT),
+    [COMBO_DOLLAR]          = COMBO(dollar,         KC_DLR),
+    [COMBO_SEMICOLON]       = COMBO(semicolon,      KC_SCLN),
+    [COMBO_DEL_WORD]        = COMBO(del_word,       C(KC_DEL)),
+    [COMBO_ESC]             = COMBO(escape,         KC_ESC),
 };
 
 // #endif
